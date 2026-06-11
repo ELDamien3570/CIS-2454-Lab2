07694848e6317115e30184c81594a0d4d20bc00e
ELDamien3570 <damienhelm@gmail.com>
2026-06-11 05:02:14 -0400

    Update display.php

--- display.php
+++ display.php
@@ -1,7 +1,7 @@
 <?php
     $pay_rate = htmlspecialchars(filter_input(INPUT_POST, 'pay_rate'));
     $hours = htmlspecialchars(filter_input(INPUT_POST, 'hours'));
-    $estimated_tax = htmlspecialchars(filter_input(INPUT_POST, 'tax_rate'));
+    $estimated_tax = htmlspecialchars(filter_input(INPUT_POST, 'estiamted_tax'));
 ?>
 
 <html>
